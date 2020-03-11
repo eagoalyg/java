95cf31f8f06cc14e1ec6a28edf764529520d5da1
eagoalyg <1787024472@qq.com>
2020-03-11 09:55:09 +0800

    文件写读操作

---
+++ 02.java读写操作/ReadStream.java
@@ -0,0 +1,41 @@
+//java文件读操作
+import java.io.*;
+
+public class ReadStream{
+    public static void main(String[] args){
+        FileInputStream fis = null;
+        InputStreamReader isr = null;
+        BufferedReader br = null;
+        try{
+            fis = new FileInputStream("dir/t.txt");
+            isr = new InputStreamReader(fis, "utf-8");
+            br = new BufferedReader(isr);
+            String line;
+            while((line = br.readLine()) != null){
+                System.out.println(line);
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }finally{
+            try{
+                br.close();
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+            
+        }
+
+    }
+
+
+    private void readFile2(){
+        try(BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream("dir/t.txt")))){
+            String line;
+            while((line = br.readLine()) != null){
+                System.out.println(line);
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+            }
+    }
+}
---
+++ 02.java读写操作/WriteStream.java
@@ -0,0 +1,46 @@
+//java文件写操作
+import java.io.*;
+
+public class WriteStream{
+    public static void main(String[] args){
+        writeFile1();
+        writeFile2();
+    }
+
+
+    private static void writeFile1(){
+        FileOutputStream fis = null;
+        OutputStreamWriter isr = null;
+        BufferedWriter br = null;
+        try{
+            fis = new FileOutputStream("dir/t.txt");
+            isr = new OutputStreamWriter(fis, "utf-8");
+            br = new BufferedWriter(isr);
+            br.write("hello");
+            br.newLine();
+            br.write("jack");
+            br.newLine();
+        }catch(Exception e){
+            e.printStackTrace();
+        }finally{
+            try{
+                br.close();
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+            
+        }
+    }
+
+
+    private static void writeFile2(){
+        try(BufferedWriter br = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("dir/t.txt")))){
+            br.write("hello");
+            br.newLine();
+            br.write("jack");
+            br.newLine();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }    
+}
