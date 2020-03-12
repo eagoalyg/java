2007249e26f75b1b3f853415a51348341ac4db5c
eagoalyg <1787024472@qq.com>
2020-03-12 22:09:37 +0800

    多线程简单实现

--- 04.java多线程/ThreadTest.java
+++ 04.java多线程/ThreadTest.java
@@ -4,12 +4,17 @@ public class ThreadTest{
 	public static void main(String[] args){
 		ThreadOne to = new ThreadOne("dick");
 		ThreadOne to1 = new ThreadOne("jack");
+		ThreadTwo tt = new ThreadTwo();
+		//使用接口实现多线程必须用Thread类来包装线程类
+		Thread t = new Thread(tt);
 		to.start();
 		to1.start();
+		t.start();
+
 	}
 }
 
-
+//使用类继承来实现多进程
 class ThreadOne extends Thread{
 	String names;
 	ThreadOne(String names){
@@ -22,5 +27,19 @@ class ThreadOne extends Thread{
 		}catch(InterruptedException e){
 			e.printStackTrace();
 		}
+		System.out.println(Thread.currentThread().getName() + "done");
+	}
+}
+
+//使用接口实现多进程
+class ThreadTwo implements Runnable{
+	public void run(){
+		System.out.println("hello" + Thread.currentThread().getName());
+		try{
+			Thread.sleep(3000);
+		}catch(InterruptedException e){
+			e.printStackTrace();
+		}
+		System.out.println(Thread.currentThread().getName() + "done");
 	}
 }
